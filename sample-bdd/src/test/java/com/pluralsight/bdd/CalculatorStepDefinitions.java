c77a3463f097696f782f67cab1344807ac5b552b
abhayrautela <abhayrautela@gmail.com>
2019-12-30 22:06:39 +0530

    Adding feature files

---
+++ sample-bdd/src/test/java/com/pluralsight/bdd/CalculatorStepDefinitions.java
@@ -0,0 +1,37 @@
+package com.pluralsight.bdd;
+
+import com.pluralsight.calculator.Calculator;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorStepDefinitions {
+
+    private Calculator calculator;
+    private Integer result;
+
+    @Given("I have a calculator")
+    public void i_have_a_calculator() {
+        // Write code here that turns the phrase above into concrete actions
+        // throw new cucumber.api.PendingException();
+        calculator = new Calculator();
+    }
+
+    @When("I add {int} and {int}")
+    public void i_add_and(Integer int1, Integer int2) {
+        // Write code here that turns the phrase above into concrete actions
+        // throw new cucumber.api.PendingException();
+        result = calculator.add(int1, int2);
+    }
+
+    @Then("I should get {int}")
+    public void i_should_get(Integer expectedResult) {
+        // Write code here that turns the phrase above into concrete actions
+        // throw new cucumber.api.PendingException();
+        assertThat(result).isEqualTo(expectedResult);
+
+    }
+
+}
---
+++ sample-bdd/src/test/java/com/pluralsight/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package com.pluralsight.calculator;
+
+public class Calculator {
+    public Integer add(Integer int1, Integer int2) {
+        return new Integer(int1.intValue() + int2.intValue());
+    }
+}
